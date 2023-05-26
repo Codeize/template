d052d34782bdc480396be6568b6df2460d4b29be
FadTheChad <fmzfads@gmail.com>
2023-05-26 20:23:03 +0500

    C L E A N

--- packages/database/src/db.ts
+++ packages/database/src/db.ts
@@ -1,111 +1,25 @@
-import { FactoryReset, Guild, PremiumGuildSlot, User, PrismaClient, Prisma, UserGuildData } from "../../../node_modules/.prisma/client"
+import { PrismaClient } from "../../../node_modules/.prisma/client"
+
+import { getFactoryResets, markFactoryReset } from "./modules/FactoryReset"
+import { deleteAllGuildSettings, getGuildData } from "./modules/Guild"
+import { deleteAllUserGuildData, getUserGuildData } from "./modules/Member"
+import { getUserData } from "./modules/User"
 
 export default class Database {
 	constructor(public db: PrismaClient) {}
 
-	public async deleteAllUserGuildData(guildId: string): Promise<void> {
-		await this.db.userGuildData.deleteMany({
-			where: {
-				guildId,
-			},
-		})
-	}
+	// MEMBER
+	public getUserGuildData = getUserGuildData
+	public deleteAllUserGuildData = deleteAllUserGuildData
 
-	public async deleteAllGuildSettings(guildId: string): Promise<void> {
-		await this.db.guild.delete({
-			where: {
-				id: guildId,
-			},
-		})
+	// GUILD
+	public getGuildData = getGuildData
+	public deleteAllGuildSettings = deleteAllGuildSettings
 
-		await this.db.guild.create({
-			data: {
-				id: guildId,
-			},
-		})
-	}
+	// USER
+	public getUserData = getUserData
 
-	public async markFactoryReset(guildId: string, executorId: string): Promise<FactoryReset> {
-		const reset = await this.db.factoryReset.create({
-			data: {
-				guildId,
-				executorId,
-				resetAt: new Date(),
-			},
-		})
-		return reset
-	}
-
-	public async getFactoryResets(guildId: string): Promise<FactoryReset[]> {
-		return await this.db.factoryReset.findMany({
-			where: {
-				guildId,
-			},
-		})
-	}
-
-	public async getGuildData(
-		guildId: string,
-		include: Prisma.GuildInclude = { premiumGuildSlots: true }
-	): Promise<
-		Guild & {
-			userData?: UserGuildData[] | undefined
-			premiumGuildSlots?: PremiumGuildSlot[] | undefined
-			factoryResets?: FactoryReset[] | undefined
-			_count?: Prisma.GuildCountOutputType | undefined
-		}
-	> {
-		if (!guildId) throw new Error("No guild ID provided")
-		const guildData = await this.db.guild.upsert({
-			where: {
-				id: guildId,
-			},
-			update: {},
-			create: {
-				id: guildId,
-			},
-			include,
-		})
-		return guildData
-	}
-
-	public async getUserData(
-		userId: string,
-		include: Prisma.UserInclude = { premiumGuildSlots: true }
-	): Promise<
-		User & {
-			premiumGuildSlots?: PremiumGuildSlot[] | undefined
-			guildData?: UserGuildData[] | undefined
-			_count?: Prisma.UserCountOutputType | undefined
-		}
-	> {
-		const userData = await this.db.user.upsert({
-			where: {
-				id: userId,
-			},
-			update: {},
-			create: {
-				id: userId,
-			},
-			include,
-		})
-		return userData
-	}
-
-	public async getUserGuildData(userId: string, guildId: string): Promise<UserGuildData> {
-		const userGuildData = await this.db.userGuildData.upsert({
-			where: {
-				userId_guildId: {
-					userId,
-					guildId,
-				},
-			},
-			update: {},
-			create: {
-				userId,
-				guildId,
-			},
-		})
-		return userGuildData
-	}
+	// FACTORY RESET
+	public getFactoryResets = getFactoryResets
+	public markFactoryReset = markFactoryReset
 }
